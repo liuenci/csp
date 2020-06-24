16f89472751aa909cb6caa92d3e71859a5e61452
liuenci <liuenci@cvte.com>
2020-06-24 11:56:56 +0800

    :lipstick: Logback 日志输出修改

--- csp-web/src/main/java/com/liuencier/csp/web/DemoController.java
+++ csp-web/src/main/java/com/liuencier/csp/web/DemoController.java
@@ -1,6 +1,7 @@
 package com.liuencier.csp.web;
 
 import com.liuencier.csp.domain.common.CommonResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: liuenci
  * @create: 2020-06-23 19:31
  **/
+@Slf4j
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
-    @GetMapping("/health")
-    public CommonResult health(String id) {
+    @GetMapping("/health/{id}")
+    public CommonResult health(@PathVariable("id") String id) {
+        log.info("心跳检测中,ID={}", id);
         return CommonResult.success("心跳检测成功", id);
     }
 }
