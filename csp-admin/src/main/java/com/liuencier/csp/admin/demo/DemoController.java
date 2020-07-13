c412b2612bc194c58e92d591ec5ac1affd5afdcc
liuenci <liuencier@gmail.com>
2020-07-13 20:58:12 +0800

    :construction: 增加多环境部署配置

--- csp-admin/src/main/java/com/liuencier/csp/admin/demo/DemoController.java
+++ csp-admin/src/main/java/com/liuencier/csp/admin/demo/DemoController.java
@@ -2,6 +2,8 @@ package com.liuencier.csp.admin.demo;
 
 import com.liuencier.csp.core.common.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
+    @Value("${profiles}")
+    private String profie;
+
     @GetMapping("/health/{id}")
     public CommonResult health(@PathVariable("id") String id) {
         log.info("心跳检测中,ID={}", id);
         return CommonResult.success("心跳检测成功", id);
+    }
+
+    @GetMapping("/profie")
+    public CommonResult profie() {
+        return CommonResult.success(profie);
     }
 }
