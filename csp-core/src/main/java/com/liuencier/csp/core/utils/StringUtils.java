5e16c1ccabfca8e6bd6aec05202acdd3e33cadea
liuenci <liuencier@gmail.com>
2020-07-23 20:02:20 +0800

    :construction: 增加 StringUtils.java

---
+++ csp-core/src/main/java/com/liuencier/csp/core/utils/StringUtils.java
@@ -0,0 +1,14 @@
+package com.liuencier.csp.core.utils;
+
+/**
+ * @program: csp
+ * @description: 字符串工具类
+ * @author: liuenci
+ * @create: 2020-07-23 20:00
+ **/
+public class StringUtils {
+
+    public static String upperCase(String str) {
+        return str == null ? null : str.toUpperCase();
+    }
+}
