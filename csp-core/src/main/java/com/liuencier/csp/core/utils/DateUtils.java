1c5466c7d78fb92c2fd197ba7a8ba9486388d106
liuenci <liuencier@gmail.com>
2020-07-27 18:38:08 +0800

    :construction: DateUtils 增加 now() api

--- csp-core/src/main/java/com/liuencier/csp/core/utils/DateUtils.java
+++ csp-core/src/main/java/com/liuencier/csp/core/utils/DateUtils.java
@@ -11,6 +11,7 @@ import java.util.Date;
  **/
 public class DateUtils {
 
+
     public static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     public static String format(Date date, String pattern) {
@@ -22,5 +23,9 @@ public class DateUtils {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         String format = simpleDateFormat.format(date);
         return format;
+    }
+
+    public static String now() {
+        return format(new Date(), DEFAULT_PATTERN);
     }
 }
