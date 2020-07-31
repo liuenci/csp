9f55536ac70e6f7acf2fafdff8a8fbbf4a9ee0f7
liuenci <liuencier@gmail.com>
2020-07-31 11:15:16 +0800

    :construction: Demo add

---
+++ csp-core/src/main/java/com/liuencier/csp/core/component/Demo.java
@@ -0,0 +1,18 @@
+package com.liuencier.csp.core.component;
+
+import java.math.BigDecimal;
+
+/**
+ * @program: csp
+ * @description:
+ * @author: liuenci
+ * @create: 2020-07-31 10:38
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal("100");
+        BigDecimal b = new BigDecimal("3");
+        BigDecimal result = a.divide(b,3, BigDecimal.ROUND_HALF_UP);
+        System.out.println("result=" + result);
+    }
+}
